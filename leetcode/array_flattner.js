090370016533440539386017a5ac562b83d73a7f
I-yan-u <iyanuajimobi12@gmail.com>
2024-04-16 23:18:01 +0100

    Add array flattener solution and description file

---
+++ leetcode/array_flattner.js
@@ -0,0 +1,35 @@
+// Solution doesn't perofrm well on Time and Memory benchmarks, but it works for all the test cases
+
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+let subArrays = [];
+let subArray = 0;
+var flat = function (arr, n) {
+    let len = subArrays.length;
+    if (len > 1 &&
+     subArrays[len - 1] === subArrays[len - 2]){
+        subArrays = [];
+        subArray = 0;
+        return arr
+    }
+    
+    if (n <= 0) {
+        return arr;
+    };
+    
+    n--;
+    const newarr = [];
+    arr.forEach((val) => {
+        if (Array.isArray(val)){
+            newarr.push(...val);
+            subArray ++;
+        } else {
+            newarr.push(val);
+        }
+    })
+    subArrays.push(subArray);
+    return flat(newarr, n);
+};
