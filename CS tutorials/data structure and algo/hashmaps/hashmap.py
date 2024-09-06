8bef6aef42d4ca665e42f0a5444ee3a71bf7a130
I-yan-u <iyanuajimobi12@gmail.com>
2024-09-07 00:30:59 +0100

    refactor: Implement hashmap data structure

---
+++ CS tutorials/data structure and algo/hashmaps/hashmap.py
@@ -0,0 +1,47 @@
+class Hashmap:
+    def __init__(self):
+        self.MAP = 10
+        self.arr = [[] for _ in range(self.MAP)]
+
+    def get_hash(self, key):
+        total = 0
+        for char in key:
+            total += ord(char)
+        return total % self.MAP
+    
+    def __setitem__(self, key, value):
+        h = self.get_hash(key)
+        found = False
+        for idx, elements in enumerate(self.arr[h]):
+            if len(elements) == 2 and elements[0] == key:
+                self.arr[h][idx] = (key, value)
+                found = True
+                break
+        if not found:
+            self.arr[h].append((key, value))
+
+    def __getitem__(self, key):
+        h = self.get_hash(key)
+        for elements in self.arr[h]:
+            if elements[0] == key:
+                return elements[1]
+
+    def __delitem__(self, key):
+        h = self.get_hash(key)
+        for elements in self.arr[h]:
+            if elements[0] == key:
+                del elements
+
+    # def __str__(self):
+    #     return f'{x: y for x}'
+            
+
+if __name__ == '__main__':
+    t = Hashmap()
+    t['march 6'] = 52
+    t['march 5'] = 200
+    t['march 3'] = 25
+    t['march 6'] = 2
+    t['march 4'] = 569
+    t['march 17'] = 55
+    print(t['march 17'])
